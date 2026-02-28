a40a4064ee9ed20af7dfba55780ac4ec6d0b0995
David Almeida <contato.davidalmeida@outlook.com>
2026-02-28 01:09:02 -0300

    🔧 Update configuration file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import pluginPrettier from "eslint-plugin-prettier";
+import prettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +16,18 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      prettier: pluginPrettier,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  prettier,
 ]);
 
 export default eslintConfig;
