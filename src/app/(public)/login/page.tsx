d39eb57840d3dd502b863761be04d2a05cfa627c
David Almeida <contato.davidalmeida@outlook.com>
2026-03-08 00:11:42 -0300

    ✅ Add: Login and Register page

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <div>
+      <span>Pagina de Acessp</span>
+      <p>Voltar para <Link href="/" className="font-bold underline">Inicio</Link></p>
+    </div>
+  ) 
+}
---
+++ src/app/(public)/register/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Register() {
+  return (
+    <div>
+      <span>Pagina de Registro</span>
+      <p>Voltar para <Link href="/" className="font-bold underline">Inicio</Link></p>
+    </div>
+  ) 
+}
